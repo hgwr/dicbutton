72381854494749ba055061a429ee9ebd92481003
Shigeru Hagiwara <hgwrsgr@gmail.com>
2010-05-06 14:07:10 +0900

    *** empty log message ***

--- build.sh
+++ build.sh
@@ -7,5 +7,17 @@ if [ \! -d build ]; then
   mkdir build
 fi
 
+git status
+echo " "
+echo -n "(build.sh) Continue ? (yes/No): "
+read ans
+if [ "$ans" != 'yes' ]; then
+  echo  "abort"
+  exit
+fi
+
 git-archive --format=tar --prefix="build/${APP}/" HEAD | tar xvf -
-# $CHROME --pack-extension="build/${APP}" --pack-extension-key="../${APP}.pem"
+rm build/adb/.gitignore
+rm build/adb/build.sh
+
+echo $CHROME --pack-extension="build/${APP}" --pack-extension-key="../${APP}.pem"
